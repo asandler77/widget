6ccc80c36bb406b7a1bf4d471c35ad2621c61c65
asandler77 <shwartz.alexey77@gmail.com>
2025-05-31 15:23:27 +0300

    blur view working properly

--- components/BluredText.tsx
+++ components/BluredText.tsx
@@ -1,20 +1,43 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React, {Component} from 'react';
+import {View, Image, Text, StyleSheet} from 'react-native';
+import {BlurView} from '@react-native-community/blur';
 
 export const BluredText = () => {
   return (
     <View style={styles.container}>
-      <Text>I AM BLUUURED</Text>
-      <Text>I'm the non blurred text because I got rendered on top</Text>
+      <Image
+        key={'blurryImage'}
+        source={{
+          uri: 'https://images.pexels.com/photos/1103970/pexels-photo-1103970.jpeg',
+        }}
+        style={styles.absolute}
+      />
+      <Text style={styles.absolute}>Hi, I am some blurred text</Text>
+      {/* in terms of positioning and zIndex-ing everything before the BlurView will be blurred */}
+      <BlurView
+        style={styles.absolute}
+        blurType="light"
+        blurAmount={10}
+        reducedTransparencyFallbackColor="white"
+      />
+      <Text>
+        I'm the non blurred text because I got rendered on top of the BlurView
+      </Text>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    justifyContent: 'center', // центр по вертикали
-    alignItems: 'center', // центр по горизонтали
-    backgroundColor: 'lightblue',
-    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    flex:1,
+  },
+  absolute: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
   },
 });
